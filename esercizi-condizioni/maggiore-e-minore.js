2b538418a2963e6c0245e431141dea26864dc7dd
ugozane <63853180+ugozane@users.noreply.github.com>
2020-04-19 20:02:34 +0200

    maggiore e minore

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -6,7 +6,13 @@
   Esempio:
     Input: a = 3, b = -1, c = 4, d = -2
     Output: maggiore = 4, minore = -2
-    
+
 
   http://www.imparareaprogrammare.it
 */
+var a = 3;
+var b = -2;
+var c= 7;
+var d = -5;
+console.log(Math.max(a,b,c,d));
+console.log(Math.min(a,b,c,d));
